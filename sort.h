cc16522b85c14542f2cf46dd24df377d585aef89
Bonziman <eloirdiwi@gmail.com>
2024-01-20 18:35:09 +0100

    added prototype to header

--- sort.h
+++ sort.h
@@ -22,4 +22,5 @@ void print_list(const listint_t *list);
 void swap(int *xp, int *yp);
 int main(void);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 #endif /* SORT */
