59ac0b0bc3c3d3437b609d120e2d3ee0305a9b3b
Bonziman <eloirdiwi@gmail.com>
2024-01-20 18:53:15 +0100

    doone

--- sort.h
+++ sort.h
@@ -23,4 +23,7 @@ void swap(int *xp, int *yp);
 int main(void);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
 #endif /* SORT */
