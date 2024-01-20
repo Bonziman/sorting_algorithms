400b4af5624a7a1e56b46d07dcce9a39bcc8c779
Bonziman <eloirdiwi@gmail.com>
2024-01-20 17:09:31 +0100

    betty style header file

--- sort.h
+++ sort.h
@@ -19,6 +19,6 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void swap(int* xp, int* yp);
+void swap(int *xp, int *yp);
 int main(void);
 #endif /* SORT */
