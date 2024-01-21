8846c7f7ef9b0cf21357cbf30f50ab932f14bd62
Bonziman <eloirdiwi@gmail.com>
2024-01-21 19:53:59 +0100

    enhanced the size if condition

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,11 +26,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	if (size < 2)
+	if (size < 1)
 	{
 		return;
 	}
-	else if (size > 2)
+	else if (size > 1)
 	{
 		for (i = 0; i < size - 1; i++)
 		{
