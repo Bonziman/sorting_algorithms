2d61d1c71ba65f1ca1a6e77e993b7bddf9768219
Bonziman <eloirdiwi@gmail.com>
2024-01-21 19:29:56 +0100

    added condition to check if size is > 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,15 +25,17 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-
-	for (i = 0; i < size - 1; i++)
+	if (size > 2)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < size - i - 1; j++)
 			{
-				swap(&array[j], &array[j + 1]);
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+				}
 			}
 		}
 	}
